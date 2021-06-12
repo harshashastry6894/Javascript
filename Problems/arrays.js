17734a9d0d7a77be9fa1cc0b410a7b3c1c3f17e0
harshashastry6894 <harshashastry6894@gmail.com>
2021-06-12 18:27:21 +0530

    * Adding js concepts

--- Problems/arrays.js
+++ Problems/arrays.js
@@ -143,12 +143,7 @@ var twoSum = function (nums, target) {
     }
 };
 
-// Given an unsorted array A of size N that contains only non-negative integers, 
-// find a continuous sub-array which adds to a given number S.
-// Example 1:
-// Input:
-// N = 5, S = 12
-// A[] = {1,2,3,7,5}
+// Input: N = 5, S = 12, A[] = {1,2,3,7,5}
 // Output: 2 4
 // Explanation: The sum of elements from 2nd position to 4th position is 12.
 function subArray(arr, n, s) {
@@ -161,3 +156,21 @@ function subArray(arr, n, s) {
         }
     }
 }
+
+//Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+//Output: 6
+//Explanation: [4,-1,2,1] has the largest sum = 6.
+var maxSubArray = function (nums) {
+    let sum = 0;
+    let maxSum = -Infinity;
+
+    if (nums.length === 0) return 0;
+    if (nums.length === 1) return nums[0]
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        maxSum = Math.max(maxSum, sum);
+        if (sum < 0) sum = 0;
+    }
+    return maxSum;
+};
