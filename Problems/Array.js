bc42a2d4df1247baf7e70d4de0ae3ac9a6df9c29
harshashastry6894 <harshashastry6894@gmail.com>
2024-06-22 21:16:31 +0530

    Update Array.js

--- Problems/Array.js
+++ Problems/Array.js
@@ -1,26 +1,23 @@
 Plm: 2 sum -> https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 Approach: 
-  1. set first value index as low and last value index as high
-  2. while low < high 
-  3. if value of low + value of high is equal to target then return [low +1, high + 1]
-  4. if value of low + value of high is less than target then increment low
-  5. if value of low + value of high is greater than target then decrement high
-  4. if while loop breaks then return empty []
+  1. declare a func pointer fc
+  2. loop through an array 
+  3. find the differnece value with target - array element
+  4. if the diff is in fc then return fc[diff] and i (fc[diff] contains the value with index)
+  5. else store the current value and it's index in fc
+  4. if for loop breaks then return  [-1, -1]
 
-  function twoSum(numbers: number[], target: number): number[] {
-    let low = 0;
-    let high = numbers.length - 1;
-
-    while (low < high) {
-        if ((numbers[low] + numbers[high]) === target) return [low + 1, high + 1]
-        if ((numbers[low] + numbers[high]) > target) {
-            high = high - 1
-        }
-        if ((numbers[low] + numbers[high]) < target) {
-            low = low + 1
+function twoSum(nums: number[], target: number): number[] {
+    const fc = {}
+    for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i]
+        if (diff in fc) {
+            return [fc[diff], i]
+        } else {
+            fc[nums[i]] = i
         }
     }
-    return []
+    return [-1, -1]
 };
 
 plm: Best Time to Buy and Sell Stock -> https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
