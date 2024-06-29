b6ca5e351bbb30b55e7eaed128c828b7421376fa
harshashastry6894 <harshashastry6894@gmail.com>
2024-06-29 22:08:12 +0530

    Update Array.js

--- Problems/Array.js
+++ Problems/Array.js
@@ -1,4 +1,4 @@
-Plm: 2 sum -> https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+1st Plm: 2 sum -> https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 Approach: 
   1. declare a func pointer fc
   2. loop through an array 
@@ -21,7 +21,7 @@ function twoSum(nums: number[], target: number): number[] {
     return [-1, -1]
 };
 
-Plm: Best Time to Buy and Sell Stock -> https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+2nd Plm: Best Time to Buy and Sell Stock -> https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 Approach: 
   1. initialize maxProfit to 0
   2. Traverse through the array and find the currentPrice by subtracting current value with previous value 
@@ -39,7 +39,7 @@ function maxProfit(prices: number[]): number {
     return maxProfit;
 };
 
-Plm: Contains Duplicate -> https://leetcode.com/problems/contains-duplicate/
+3rd Plm: Contains Duplicate -> https://leetcode.com/problems/contains-duplicate/
 Sample: [1,2,3,1] -> true
 function containsDuplicate(nums: number[]): boolean {
     let fc = {};
@@ -53,7 +53,7 @@ function containsDuplicate(nums: number[]): boolean {
     return false
 };
 
-Plm: Product of Array Except Self -> https://leetcode.com/problems/product-of-array-except-self/description/
+4th Plm: Product of Array Except Self -> https://leetcode.com/problems/product-of-array-except-self/description/
 Sample: [1,2,3,4] -> [24, 12, 8, 6]
 function productExceptSelf(nums: number[]): number[] {
     const length = nums.length;
@@ -73,7 +73,7 @@ function productExceptSelf(nums: number[]): number[] {
     return result;
 };
 
-Plm: Maximum Subarray -> https://leetcode.com/problems/maximum-subarray/description/
+5th Plm: Maximum Subarray -> https://leetcode.com/problems/maximum-subarray/description/
 Sample: [-2,1,-3,4,-1,2,1,-5,4] -> The subarray [4,-1,2,1] has the largest sum 6.
 function maxSubArray(nums: number[]): number {
     let sum = 0, max = nums[0]
@@ -85,7 +85,7 @@ function maxSubArray(nums: number[]): number {
      return max
 }
 
-Plm: Maximum product subarray -> https://leetcode.com/problems/maximum-product-subarray/description/
+6th Plm: Maximum product subarray -> https://leetcode.com/problems/maximum-product-subarray/description/
 Sample: [2,3,-2,4] -> [2,3] has the largest product 6.
 function maxProduct(nums: number[]): number {
     let prefixProduct = 1;
