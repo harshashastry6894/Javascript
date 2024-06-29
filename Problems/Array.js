f79567c766561f44c927518e472d505f20d22983
harshashastry6894 <harshashastry6894@gmail.com>
2024-06-29 22:07:12 +0530

    Update Array.js

--- Problems/Array.js
+++ Problems/Array.js
@@ -84,3 +84,22 @@ function maxSubArray(nums: number[]): number {
     }
      return max
 }
+
+Plm: Maximum product subarray -> https://leetcode.com/problems/maximum-product-subarray/description/
+Sample: [2,3,-2,4] -> [2,3] has the largest product 6.
+function maxProduct(nums: number[]): number {
+    let prefixProduct = 1;
+    let suffixProduct = 1;
+    let maxProd = nums[0];
+    const n = nums.length;
+    for(let i=0,j=n-1; i<n && j>=0;i++,j--){
+        prefixProduct *= nums[i];
+        suffixProduct *= nums[j];
+        maxProd = Math.max(prefixProduct,maxProd);
+        maxProd = Math.max(suffixProduct,maxProd);
+        if(nums[i]==0)prefixProduct =1;
+        if(nums[j]==0)suffixProduct =1;
+
+    }
+    return maxProd;
+};
