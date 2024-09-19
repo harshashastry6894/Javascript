150c7de2052e83911132c7a0b3979099c1805cea
Harsha S <harsha-s@pluralsight.com>
2024-09-19 19:11:46 +0530

    Added basic plms

---
+++ Problems/Basics/math.js
@@ -0,0 +1,58 @@
+// Extraction of a number is done by %
+const countDigits = (num) => {
+    let count = 0;
+    while (num > 0) {
+      count++;
+      num = Math.floor(num / 10);
+    }
+    return count;
+  };
+  
+  const reverseNumber = (num) => {
+    let rev = 0;
+    while (num > 0) {
+      rem = num % 10;
+      num = Math.floor(num / 10);
+      // Since diving by 10, we need to multiply by 10 to reverse it
+      rev = rev * 10 + rem;
+    }
+    return rev;
+  };
+  
+  const checkPalindrome = (num) => {
+    return num === reverseNumber(num);
+  };
+  
+  const printAllDivisor = (num) => {
+    for (let i = 1; i <= num; i++) {
+      if (num % i === 0) console.log(i);
+    }
+  };
+  
+  const isPrimeNumber = (num) => {
+    let count = 0;
+    for (let i = 1; i <= num; i++) {
+      if (num % i === 0) count++;
+    }
+    if (count === 2) {
+      return "Prime number";
+    } else {
+      return "Not a prime number";
+    }
+  };
+  
+  const gcdorhcf = (n1, n2) => {
+    const min = Math.min(n1, n2);
+    for (let i = min; i >= 1; i++) {
+      if (n1 % i === 0 && n2 % i === 0) {
+        return i;
+      }
+    }
+  };
+  
+  // console.log(countDigits(7789));
+  // console.log(reverseNumber(7789));
+  // console.log(checkPalindrome(7877));
+  // console.log(isPrimeNumber(8));
+  console.log(gcdorhcf(3, 12));
+  
---
+++ Problems/Basics/recursion.js
@@ -0,0 +1,48 @@
+const print = (i, n) => {
+    if (i > n) return;
+    console.log(i);
+    print(i + 1, n);
+  };
+  
+  const sum = (n) => {
+    if (n === 0) return 0;
+    return n + sum(n - 1);
+  };
+  
+  const factorial = (n) => {
+    if (n === 0) return 1; // while product return 1 otherwise everything will become zero
+    return n * factorial(n - 1);
+  };
+  
+  const fibonacci = (n) => {
+    if (n === 0 || n === 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  };
+  
+  const reverseArray = (arr, start, end) => {
+    if (start < end) {
+      const temp = arr[end];
+      arr[end] = arr[start];
+      arr[start] = temp;
+      reverseArray(arr, start + 1, end - 1);
+    }
+    return arr;
+  };
+  
+  const reverseStringIterative = (str) => {
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+      reversed += str[i];
+    }
+    return reversed;
+  };
+  
+  // print(1, 10);
+  // console.log(sum(3));
+  // console.log(factorial(3));
+  // console.log(reverseArray([1, 2, 3, 4], 0, 3));
+  // console.log(reverseArray("harsha", 0, 3));
+  // console.log(fibonacci(8));
+  
+  // 0 1 1 2 3 5
+  
